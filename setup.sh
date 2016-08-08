9e9ba850a74e7893f59868fbbc0853de3bbd1d80
fsword <li.jianye@gmail.com>
2016-08-08 15:34:23 +0800

    create a dummy user at first

--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ contact_details=${contact_details:-no@email}
 
 sed -ie "s/\$INSTANCE_NAME/$instance_name/" processes.json
 sed -ie "s/\$CONTACT_DETAILS/$contact_details/" processes.json
+
+echo 'dummy user' > pwd.txt
+geth --password pwd.txt account new
